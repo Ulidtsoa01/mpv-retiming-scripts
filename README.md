dd3cb7d6edfa3e021ca81bd32c2f9b89c41b4ced
Ulidtsoa01 <22529166+Ulidtsoa01@users.noreply.github.com>
2024-10-26 23:53:25 -0400

    rewrite in lua and add alt+u

--- README.md
+++ README.md
@@ -2,13 +2,16 @@
 
 ### snap-to-secondary-sub
 
-NOTE: Subtitle tracks don't always begin or end in the same places. It is suggested to keep the secondary sub visible while retiming with this script.
+NOTE: Different subtitle tracks don't always begin or end in the same places. It is suggested to keep the secondary sub visible while retiming with this script.
 
 Adds keybinds to make it easier to retime subs to match the timing of the internal subs in a video file:
 
-- `Alt+c`: Set the sub-delay so the start time of the current subtitle matches the start time of the secondary subtitle.
+- `Alt+c`: Set the sub-delay so the start time of the primary subtitle matches the start time of the secondary subtitle.
 - `Alt+LEFT`: Execute `sub-step -1` (CTRL+SHIFT+LEFT), then execute what `Alt+c` does.
 - `Alt+RIGHT`: Execute `sub-step 1` (CTRL+SHIFT+RIGHT), then execute what `Alt+c` does.
+- `Alt+u`: Swap primary subtitle and secondary subtitle with each other.
+
+### Reccomended mpv settings
 
 To take full advantage of this script, the following settings and keybinds are helpful.
 In `mpv.conf`:
---
+++ snap-to-secondary-sub.lua
@@ -0,0 +1,78 @@
+SNAP_KEY = 'alt+c'
+SNAP_LEFT_KEY = 'alt+LEFT'
+SNAP_RIGHT_KEY = 'alt+RIGHT'
+SWAP_KEY = 'alt+u'
+
+local mp = require 'mp'
+
+local function showMessage(message, persist)
+  local ass_start = mp.get_property_osd('osd-ass-cc/0')
+  local ass_stop = mp.get_property_osd('osd-ass-cc/1')
+  mp.osd_message(ass_start..'{\\fs16}'..message..ass_stop, persist and 999 or 2);
+end
+
+local function snapSubToSecondarySub()
+  local sub_selected = mp.get_property("current-tracks/sub/selected")
+  local secondary_sub_selected = mp.get_property("current-tracks/sub2/selected")
+  if sub_selected and secondary_sub_selected then
+    local subStart = mp.get_property_native('sub-start');
+    local secondarySubStart = mp.get_property_native('secondary-sub-start');
+    local diff = secondarySubStart - subStart;
+    mp.set_property('sub-delay', diff);
+    local message = 'Sub delay: '..math.floor(diff*1000+.5)..'ms';
+    showMessage(message);
+  else
+    local sub_delay = mp.get_property_native('sub-delay')
+    showMessage('Sub delay: '..math.floor(sub_delay*1000+.5)..'ms\\N✘No sub or secondary sub selected');
+  end
+end
+
+local function snapLeftSubToSecondarySub()
+  mp.command('no-osd sub-step -1')
+  snapSubToSecondarySub()
+end
+
+local function snapRightSubToSecondarySub()
+  mp.command('no-osd sub-step 1')
+  snapSubToSecondarySub()
+end
+
+local function format_track_switch_message(id , lang, title)
+  local text = '(%id%) %lang% ("%title%")\\N'
+  text = text:gsub('%%(%a+)%%', { id = id, lang = lang, title = title})
+  return text
+end
+
+local function swapSubWithSecondarySub()
+  local sub_selected = mp.get_property("current-tracks/sub/selected")
+  local sub2_selected = mp.get_property("current-tracks/sub2/selected")
+  local sub_id = mp.get_property_number("current-tracks/sub/id") or ""
+  local sub2_id = mp.get_property_number("current-tracks/sub2/id") or ""
+  local sub_lang = mp.get_property("current-tracks/sub/lang") or "unknown"
+  local sub2_lang = mp.get_property("current-tracks/sub2/lang") or "unknown"
+  local sub_title = mp.get_property("current-tracks/sub/title") or mp.get_property("current-tracks/sub/external-filename") or ""
+  local sub2_title = mp.get_property("current-tracks/sub2/title") or mp.get_property("current-tracks/sub2/external-filename") or ""
+  local sub_swapped = false
+  local message1 = ''
+  local message2 = ''
+  
+  if sub_selected then
+    mp.set_property("sid", "no")
+    mp.set_property("secondary-sid", sub_id)
+    sub_swapped = true
+    message2 = "Secondary subtitles: "..format_track_switch_message(sub_id, sub_lang, sub_title)
+  end
+  if sub2_selected then
+    if not sub_swapped then
+      mp.set_property("secondary-sid", "no")
+    end
+    mp.set_property("sid", sub2_id)
+    message1 = "Subtitles: "..format_track_switch_message(sub2_id, sub2_lang, sub2_title)
+  end
+  showMessage(message1..message2)
+end
+
+mp.add_key_binding(SNAP_KEY, 'sub-to-sec-sub', snapSubToSecondarySub);
+mp.add_key_binding(SNAP_LEFT_KEY, 'left-sub-to-sec-sub', snapLeftSubToSecondarySub);
+mp.add_key_binding(SNAP_RIGHT_KEY, 'right-sub-to-sec-sub', snapRightSubToSecondarySub);
+mp.add_key_binding(SWAP_KEY, 'swap-sub-with-sec-sub', swapSubWithSecondarySub);
